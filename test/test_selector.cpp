698b0a4c4ea5b22fda82a1e2be5af104c8e7cd9f
xueyuan <1920106343@qq.com>
2018-12-04 10:46:44 +0800

    add selector test

--- test/test_selector.cpp
+++ test/test_selector.cpp
@@ -1,20 +1,18 @@
-#include <iostream>
 #include "selector.h"
 #include "bus.h"
 
-using namespace std;
-
 int main() {
-    base::BusBase _in_0_(8);
-    base::BusBase _in_1_(8);
-    base::BusBase _in_2_(8);
-    base::BusBase _in_3_(8);
-    base::BusBase _in_4_(8);
-    base::BusBase _in_5_(8);
-    base::BusBase _in_6_(8);
-    base::BusBase _in_7_(8);
-    base::BusBase _out_(8);
-    base::BusBase _control_(32);
+    unsigned long _data_width = 8;
+    base::BusBase _in_0_(_data_width);
+    base::BusBase _in_1_(_data_width);
+    base::BusBase _in_2_(_data_width);
+    base::BusBase _in_3_(_data_width);
+    base::BusBase _in_4_(_data_width);
+    base::BusBase _in_5_(_data_width);
+    base::BusBase _in_6_(_data_width);
+    base::BusBase _in_7_(_data_width);
+    base::BusBase _out_(_data_width);
+    base::BusBase _control_(16);
 
     _in_0_.named("Input0");
     _in_1_.named("Input1");
@@ -27,10 +25,10 @@ int main() {
     _out_.named("Output");
     _control_.named("Control");
 
-    unsigned long _ins_offset = 0b0011110000000;
+    unsigned long _ins_active_ = 0b0011110000000;
 
     base::SelectorBase s(&_in_0_, &_in_1_, &_in_2_, &_in_3_, 
-            &_in_4_, &_in_5_, &_in_6_, &_in_7_, &_out_, &_control_, _ins_offset);
+            &_in_4_, &_in_5_, &_in_6_, &_in_7_, &_out_, &_control_, _ins_active_);
     s.named("Selector");
 
     //初始化
@@ -43,7 +41,9 @@ int main() {
     _in_6_.write(0b00000000);
     _in_7_.write(0b00000000);
     _out_.write(0b00000000);
-    _control_.write(SELECTOR_ROUTE_1 << base::_active_bits_offset(_ins_offset));
+    _control_.write(SELECTOR_ROUTE_1 << base::_active_bits_offset(_ins_active_));
+    //_control_.debug("*---");
+    //cout << "*---" << base::_bin_str(base::_extract_instruction(_control_.read(), _ins_active_), base::_active_bits_size(_ins_active_)) << endl;
     s();
     s.debug("+---");
     _in_0_.debug("o---");
@@ -57,28 +57,28 @@ int main() {
     _out_.debug("o---");
     _control_.debug("o---");
 
-    //_in_0_.write(0b00000000);
-    //_in_1_.write(0b00000000);
-    //_in_2_.write(0b00000000);
-    //_in_3_.write(0b00010101);
-    //_in_4_.write(0b00000000);
-    //_in_5_.write(0b00000000);
-    //_in_6_.write(0b00000000);
-    //_in_7_.write(0b00000000);
-    //_out_.write(0b00000000);
-    //_control_.write(SELECTOR_ROUTE_3 << base::_active_bits_offset(_ins_offset));
-    //s();
-    //s.debug("+---");
-    //_in_0_.debug("o---");
-    //_in_1_.debug("o---");
-    //_in_2_.debug("o---");
-    //_in_3_.debug("o---");
-    //_in_4_.debug("o---");
-    //_in_5_.debug("o---");
-    //_in_6_.debug("o---");
-    //_in_7_.debug("o---");
-    //_out_.debug("o---");
-    //_control_.debug("o---");
+    _in_0_.write(0b00000000);
+    _in_1_.write(0b00000000);
+    _in_2_.write(0b00000000);
+    _in_3_.write(0b00010101);
+    _in_4_.write(0b00000000);
+    _in_5_.write(0b00000000);
+    _in_6_.write(0b00000000);
+    _in_7_.write(0b00000000);
+    _out_.write(0b00000000);
+    _control_.write(SELECTOR_ROUTE_3 << base::_active_bits_offset(_ins_active_));
+    s();
+    s.debug("+---");
+    _in_0_.debug("o---");
+    _in_1_.debug("o---");
+    _in_2_.debug("o---");
+    _in_3_.debug("o---");
+    _in_4_.debug("o---");
+    _in_5_.debug("o---");
+    _in_6_.debug("o---");
+    _in_7_.debug("o---");
+    _out_.debug("o---");
+    _control_.debug("o---");
 
     return 0;
 }
